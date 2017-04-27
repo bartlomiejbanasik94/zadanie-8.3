20ca466a7645c925a380d75da1fd379c6308c5f5
bartlomiej.bansik.94 <bartlomiej.banasik.94@gmail.com>
2017-04-27 18:06:07 +0200

    problem with js file solved

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-// var name = prompt('Wpisz swoje imię');
-// alert('Witaj, ' + name);
-// console.log('Witaj, ' + name);
+// Naprawdę to robimy ?
+var name = prompt('Wpisz swoje imię');
+//alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+//Tak Bartku, dzięki tej obszernej wiedzy już wiesz jak używać komentarzy
+//Zajebiście !!!
