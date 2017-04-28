3645a2688a575bfa44a5422bb79d9787d0c3407d
bartlomiej.bansik.94 <bartlomiej.banasik.94@gmail.com>
2017-04-28 11:01:30 +0200

    added triangleArea on JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
-// Naprawdę to robimy ?
-var name = prompt('Wpisz swoje imię');
-//alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
-//Tak Bartku, dzięki tej obszernej wiedzy już wiesz jak używać komentarzy
-//Zajebiście !!!
+// Triangle Area counting
+
+var triangleA = 0;
+var triangleH = 0;
+
+
+alert ('Witaj ! Ten program liczy pole trójkąta. Możemy zaczynać ?');
+triangleA = prompt('Podaj długość boku');
+triangleH = prompt('Podaj wysokość');
+
+triangleArea = (triangleA*triangleH)/2;
+
+console.log('Pole trójkąta o podstawie a: ' + triangleA + ' i wysokości h: ' + triangleH + ' wynosi: ' + triangleArea);
+alert ('Pole trójkąta o podstawie a: ' + triangleA + ' i wysokości h: ' + triangleH + ' wynosi: ' + triangleArea)
